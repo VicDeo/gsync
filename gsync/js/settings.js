d8c44e6324b02022bf95d2d00f1153cd8fe9eed4
Victor Dubiniuk <victor.dubiniuk@gmail.com>
2013-03-01 21:28:17 +0200

    Update copyright

--- gsync/js/settings.js
+++ gsync/js/settings.js
@@ -2,7 +2,7 @@
  * ownCloud - gsync plugin
  * 
  * @author Victor Dubiniuk
- * @copyright 2012 Victor Dubiniuk victor.dubiniuk@gmail.com
+ * @copyright 2012-2013 Victor Dubiniuk victor.dubiniuk@gmail.com
  * 
  * This file is licensed under the Affero General Public License version 3 or
  * later.
