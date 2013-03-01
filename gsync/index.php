9cee532b5dcb7d48d998758850f1057718b0bbe0
Victor Dubiniuk <victor.dubiniuk@gmail.com>
2013-03-01 23:55:08 +0200

    Add namespace and constants to controller

--- gsync/index.php
+++ gsync/index.php
@@ -10,27 +10,28 @@
  * later.
  */
 
-OCP\User::checkLoggedIn();
-OCP\App::checkAppEnabled('contacts');
-OCP\App::checkAppEnabled('gsync');
+namespace OCA_Gsync;
+
+App::initController();
 
 //Check if we are redirected here with permanent sync request
-if (isset($_GET['code'])){   
+if (isset($_GET['code'])){
 	$code = preg_replace('/[^0-9a-zA-Z\.\-_\/]*/i', '', $_GET['code']);
-	$response = OCA_Gsync\Request::getTokenByCode($code);
-	$gogelSaid = json_decode($response, true);
-	OCP\Util::writeLog('Gsync', 'Gogle ssaid.' . $response, OCP\Util::DEBUG);
-	if (@$gogelSaid['refresh_token']){
-		OCP\Config::setUserValue(OCP\User::getUser(), 'gsync', 'refresh_token', $gogelSaid['refresh_token']);
+	$response = Request::getTokenByCode($code);
+	$respData = json_decode($response, true);
+	\OCP\Util::writeLog(App::APP_ID, 'Goglesaid.' . $response, \OCP\Util::DEBUG);
+	if (@$respData[App::GOOGLE_REFRESH_TOKEN]){
+		App::setRefreshToken($respData[App::GOOGLE_REFRESH_TOKEN]);
 	}
-	
+
 	//Dirty hack. 
 	//It's not possible to redirect with server headers at this point
-	echo '<script>window.location="' . OCP\Util::linkTo('settings', 'personal.php') . '";</script>';
+	echo '<script>window.location="' . \OCP\Util::linkTo('settings', 'personal.php') . '";</script>';
 	exit();
 
 } else {
-	OCP\Util::addScript('gsync', 'sync');
-	$tmpl = new OCP\Template('gsync', 'index', 'user');
+	\OCP\Util::addScript(App::APP_ID, 'sync');
+	$tmpl = new \OCP\Template(App::APP_ID, 'index', 'user');
 	$tmpl->printPage();
+
 }
