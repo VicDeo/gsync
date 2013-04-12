e6787aaa0263c01e19f6d3759f2bc12041b4f984
psych0d0g <bigluks@gmail.com>
2013-04-12 21:48:56 +0200

    Revert space intention to tab intention

--- lib/contact.php
+++ lib/contact.php
@@ -2,226 +2,226 @@
 
 /**
  * ownCloud - gsync plugin
- *
+ * 
  * @author Victor Dubiniuk
  * @copyright 2012-2013 Victor Dubiniuk victor.dubiniuk@gmail.com
- *
+ * 
  * This file is licensed under the Affero General Public License version 3 or
  * later.
- *
- *
+ * 
+ * 
  * Store Contact details
- *
+ * 
  * @package Gsync
  */
 
 namespace OCA_Gsync;
 
 class Contact {
-        //Do not sync contacts that was synced in last 10 minutes
+	//Do not sync contacts that was synced in last 10 minutes
 
-        const UPDATE_THRESHOLD = 600;
-        const CONTACT_FULLNAME = 'FN';
-        const CONTACT_NAME = 'N';
-        const CONTACT_NICKNAME = 'NICKNAME';
-        const CONTACT_ORGANIZATION = 'ORG';
-        const CONTACT_EMAIL = 'EMAIL';
-        const CONTACT_PHONE = 'TEL';
-        const CONTACT_ADDRESS = 'ADR';
-        const CONTACT_BIRTHDAY = 'BDAY';
-        const CONTACT_PHOTO = 'PHOTO';
-        const CONTACT_CATEGORIES = 'CATEGORIES';
-        const CONTACT_NOTE = 'NOTE';
-        const CONTACT_GID = 'GID';
+	const UPDATE_THRESHOLD = 600;
+	const CONTACT_FULLNAME = 'FN';
+	const CONTACT_NAME = 'N';
+	const CONTACT_NICKNAME = 'NICKNAME';
+	const CONTACT_ORGANIZATION = 'ORG';
+	const CONTACT_EMAIL = 'EMAIL';
+	const CONTACT_PHONE = 'TEL';
+	const CONTACT_ADDRESS = 'ADR';
+	const CONTACT_BIRTHDAY = 'BDAY';
+	const CONTACT_PHOTO = 'PHOTO';
+	const CONTACT_CATEGORIES = 'CATEGORIES';
+	const CONTACT_NOTE = 'NOTE';
+	const CONTACT_GID = 'GID';
 
-        private static $_propertyStrings = array(
-                self::CONTACT_NAME,
-                self::CONTACT_FULLNAME,
-                self::CONTACT_GID,
-                self::CONTACT_NICKNAME,
-                self::CONTACT_ORGANIZATION,
-                self::CONTACT_BIRTHDAY,
-                self::CONTACT_NOTE
-        );
-        private static $_bookId = null;
+	private static $_propertyStrings = array(
+		self::CONTACT_NAME,
+		self::CONTACT_FULLNAME,
+		self::CONTACT_GID,
+		self::CONTACT_NICKNAME,
+		self::CONTACT_ORGANIZATION,
+		self::CONTACT_BIRTHDAY,
+		self::CONTACT_NOTE
+	);
+	private static $_bookId = null;
 
-        /**
-         * Get contacts and store them
-         * @param string $accessToken
-         */
-        public static function import($accessToken) {
-                if (!$accessToken){
-                        App::log('Import attempt. Access token is empty', \OCP\Util::DEBUG);
-                        return;
-                }
-                Request::setAccessToken($accessToken);
+	/**
+	 * Get contacts and store them
+	 * @param string $accessToken
+	 */
+	public static function import($accessToken) {
+		if (!$accessToken){
+			App::log('Import attempt. Access token is empty', \OCP\Util::DEBUG);
+			return;
+		}
+		Request::setAccessToken($accessToken);
 
-                App::log('Firing contacts request', \OCP\Util::DEBUG);
+		App::log('Firing contacts request', \OCP\Util::DEBUG);
 
-                $respData = Request::getContactsFeed();
-                $feed = json_decode($respData, true);
-                $feed = @$feed['feed']['entry'];
+		$respData = Request::getContactsFeed();
+		$feed = json_decode($respData, true);
+		$feed = @$feed['feed']['entry'];
 
-                App::log('Got response from Google. Items count: ' . count($feed), \OCP\Util::DEBUG);
+		App::log('Got response from Google. Items count: ' . count($feed), \OCP\Util::DEBUG);
 
-                if (is_array($feed) && count($feed)) {
-                        self::parseFeed($feed);
-                }
-        }
+		if (is_array($feed) && count($feed)) {
+			self::parseFeed($feed);
+		}
+	}
 
-        /**
-         * Store feeds into the Contacts
-         * @param array $feed
-         */
-        public static function parseFeed($feed) {
-                $userid = \OCP\User::getUser();
+	/**
+	 * Store feeds into the Contacts
+	 * @param array $feed 
+	 */
+	public static function parseFeed($feed) {
+		$userid = \OCP\User::getUser();
 
-                foreach ($feed as $source) {
+		foreach ($feed as $source) {
+			
+			$entry = Adapter::translateContact($source);
 
-                        $entry = Adapter::translateContact($source);
+			if (isset($entry[self::CONTACT_GID]) && !empty($entry[self::CONTACT_GID])) {
+				$oldContactId = self::findByGid($userid, $entry[self::CONTACT_GID]);
+				if ($oldContactId) {
+					//If exists and should not be updated - skip
+					if (self::needUpdate($oldContactId)) {
+						$vcard = self::toVcard($entry);
+						\OCA\Contacts\VCard::edit($oldContactId, $vcard);
+					}
+					continue;
+				}
+			}
+			
+			$vcard = self::toVcard($entry);
+			$bookid = self::getBookId($userid);
+			\OCA\Contacts\VCard::add($bookid, $vcard);
+		}
+		\OCA\Contacts\App::getCategories();
+	}
 
-                        if (isset($entry[self::CONTACT_GID]) && !empty($entry[self::CONTACT_GID])) {
-                                $oldContactId = self::findByGid($userid, $entry[self::CONTACT_GID]);
-                                if ($oldContactId) {
-                                        //If exists and should not be updated - skip
-                                        if (self::needUpdate($oldContactId)) {
-                                                $vcard = self::toVcard($entry);
-                                                \OCA\Contacts\VCard::edit($oldContactId, $vcard);
-                                        }
-                                        continue;
-                                }
-                        }
+	/**
+	 * Init address book Id to save new Contacts
+	 * @param string $userid
+	 * @return int bookId
+	 */
+	public static function getBookId($userid) {
+		if (!self::$_bookId) {
+			self::$_bookId = \OCA\Contacts\Addressbook::add($userid, 'sync on ' . date('jS \of F Y h:i:s A'), null);
+			\OCA\Contacts\Addressbook::setActive(self::$_bookId, 1);
+		}
+		return self::$_bookId;
+	}
 
-                        $vcard = self::toVcard($entry);
-                        $bookid = self::getBookId($userid);
-                        \OCA\Contacts\VCard::add($bookid, $vcard);
-                }
-                \OCA\Contacts\App::getCategories();
-        }
+	/**
+	 * Check if the contact already exists and has't updated within threshold time.
+	 * @param type $userid
+	 * @param type $gid
+	 * @return int contactId
+	 */
+	public static function findByGid($userid, $gid) {
+		$stmt = \OC_DB::prepare("SELECT cc.id FROM *PREFIX*contacts_cards AS cc
+			INNER JOIN *PREFIX*contacts_addressbooks AS ca ON cc.addressbookid=ca.id AND ca.userid = ? WHERE cc.carddata LIKE ?");
+		$result = $stmt->execute(array($userid, "%$gid%"));
 
-        /**
-         * Init address book Id to save new Contacts
-         * @param string $userid
-         * @return int bookId
-         */
-        public static function getBookId($userid) {
-                if (!self::$_bookId) {
-                        self::$_bookId = \OCA\Contacts\Addressbook::add($userid, 'sync on ' . date('jS \of F Y h:i:s A'), null);
-                        \OCA\Contacts\Addressbook::setActive(self::$_bookId, 1);
-                }
-                return self::$_bookId;
-        }
+		return $result->fetchOne();
+	}
 
-        /**
-         * Check if the contact already exists and has't updated within threshold time.
-         * @param type $userid
-         * @param type $gid
-         * @return int contactId
-         */
-        public static function findByGid($userid, $gid) {
-                $stmt = \OC_DB::prepare("SELECT cc.id FROM *PREFIX*contacts_cards AS cc
-                        INNER JOIN *PREFIX*contacts_addressbooks AS ca ON cc.addressbookid=ca.id AND ca.userid = ? WHERE cc.carddata LIKE ?");
-                $result = $stmt->execute(array($userid, "%$gid%"));
+	/**
+	 * Check if the Contact hasn't been modified recently
+	 * @param int $contactId
+	 * @return bool
+	 */
+	public static function needUpdate($contactId) {
+		$modifiedBefore = time() - self::UPDATE_THRESHOLD;
+		$stmt = \OC_DB::prepare("SELECT count(id) FROM *PREFIX*contacts_cards WHERE id=? AND lastmodified<?");
+		$result = $stmt->execute(array($contactId, $modifiedBefore));
+		return $result->fetchOne();
+	}
 
-                return $result->fetchOne();
-        }
+	/**
+	 * Parse contact details into the Contact object
+	 * @param array $contact
+	 * @return Sabre\VObject\Component 
+	 */
+	public static function toVcard($contact) {
+		$vcard = new \Sabre\VObject\Component('VCARD');
+		$vcard->setUID();
 
-        /**
-         * Check if the Contact hasn't been modified recently
-         * @param int $contactId
-         * @return bool
-         */
-        public static function needUpdate($contactId) {
-                $modifiedBefore = time() - self::UPDATE_THRESHOLD;
-                $stmt = \OC_DB::prepare("SELECT count(id) FROM *PREFIX*contacts_cards WHERE id=? AND lastmodified<?");
-                $result = $stmt->execute(array($contactId, $modifiedBefore));
-                return $result->fetchOne();
-        }
+		$vcard = self::_addPropertyStrings($contact, $vcard);
 
-        /**
-         * Parse contact details into the Contact object
-         * @param array $contact
-         * @return Sabre\VObject\Component
-         */
-        public static function toVcard($contact) {
-                $vcard = new \Sabre\VObject\Component('VCARD');
-                $vcard->setUID();
+		if (isset($contact[self::CONTACT_PHONE])) {
+			foreach ($contact[self::CONTACT_PHONE] as $phone) {
+				if (!isset($phone['value'])) {
+					continue;
+				}
 
-                $vcard = self::_addPropertyStrings($contact, $vcard);
+				$vcard->addProperty(self::CONTACT_PHONE, $phone['value']);
+				$line = count($vcard->children) - 1;
+				foreach ($phone['type'] as $type) {
+					$vcard->children[$line]->parameters[] = new \Sabre\VObject\Parameter('TYPE', $type);
+				}
+			}
+		}
 
-                if (isset($contact[self::CONTACT_PHONE])) {
-                        foreach ($contact[self::CONTACT_PHONE] as $phone) {
-                                if (!isset($phone['value'])) {
-                                        continue;
-                                }
+		if (isset($contact[self::CONTACT_CATEGORIES])) {
+			$categories = array();
+			foreach ($contact[self::CONTACT_CATEGORIES] as $categoryId) {
 
-                                $vcard->addProperty(self::CONTACT_PHONE, $phone['value']);
-                                $line = count($vcard->children) - 1;
-                                foreach ($phone['type'] as $type) {
-                                        $vcard->children[$line]->parameters[] = new \Sabre\VObject\Parameter('TYPE', $type);
-                                }
-                        }
-                }
+				$categoryData = Request::getGroupDetails($categoryId);
+				preg_match('/<title>(.*)<\/title>/i', $categoryData, $matches);
+				if (@$matches[1]) {
+					$categories[] = $matches[1];
+				}
+			}
+			if (count($categories)) {
+				$vcard->setString(self::CONTACT_CATEGORIES, implode(',', $categories));
+			}
+		}
 
-                if (isset($contact[self::CONTACT_CATEGORIES])) {
-                        $categories = array();
-                        foreach ($contact[self::CONTACT_CATEGORIES] as $categoryId) {
+		if (isset($contact[self::CONTACT_ADDRESS])) {
+			foreach ($contact[self::CONTACT_ADDRESS] as $address) {
+				$vcard->addProperty(self::CONTACT_ADDRESS, $address);
+				$line = count($vcard->children) - 1;
+				$vcard->children[$line]->parameters[] = new \Sabre\VObject\Parameter('TYPE', $address['type']);
+			}
+		}
 
-                                $categoryData = Request::getGroupDetails($categoryId);
-                                preg_match('/<title>(.*)<\/title>/i', $categoryData, $matches);
-                                if (@$matches[1]) {
-                                        $categories[] = $matches[1];
-                                }
-                        }
-                        if (count($categories)) {
-                                $vcard->setString(self::CONTACT_CATEGORIES, implode(',', $categories));
-                        }
-                }
+		if (isset($contact[self::CONTACT_EMAIL])) {
+			foreach ($contact[self::CONTACT_EMAIL] as $email) {
+				$vcard->addProperty(self::CONTACT_EMAIL, $email['value']);
+				  $line = count($vcard->children) - 1;
+				  $vcard->children[$line]->parameters[] = new \Sabre\VObject\Parameter('TYPE', $email['type']);
 
-                if (isset($contact[self::CONTACT_ADDRESS])) {
-                        foreach ($contact[self::CONTACT_ADDRESS] as $address) {
-                                $vcard->addProperty(self::CONTACT_ADDRESS, $address);
-                                $line = count($vcard->children) - 1;
-                                $vcard->children[$line]->parameters[] = new \Sabre\VObject\Parameter('TYPE', $address['type']);
-                        }
-                }
+			}
+		}
 
-                if (isset($contact[self::CONTACT_EMAIL])) {
-                        foreach ($contact[self::CONTACT_EMAIL] as $email) {
-                                $vcard->addProperty(self::CONTACT_EMAIL, $email['value']);
-                                  $line = count($vcard->children) - 1;
-                                  $vcard->children[$line]->parameters[] = new \Sabre\VObject\Parameter('TYPE', $email['type']);
+		if (isset($contact[self::CONTACT_PHOTO]) && !empty($contact[self::CONTACT_PHOTO])) {
+			$data = Request::getContactImage($contact[self::CONTACT_PHOTO]);
+			$img = new \OC_Image();
+			if ($img->loadFromData($data)) {
+				$vcard->addProperty(self::CONTACT_PHOTO, $img->__toString(), array('ENCODING' => 'b', 'TYPE' => $img->mimeType()));
+			} else {
+				App::log('Unable to parse the image provided by Google. ', \OCP\Util::WARN);
+			}
+		}
 
-                        }
-                }
+		return $vcard;
+	}
 
-                if (isset($contact[self::CONTACT_PHOTO]) && !empty($contact[self::CONTACT_PHOTO])) {
-                        $data = Request::getContactImage($contact[self::CONTACT_PHOTO]);
-                        $img = new \OC_Image();
-                        if ($img->loadFromData($data)) {
-                                $vcard->addProperty(self::CONTACT_PHOTO, $img->__toString(), array('ENCODING' => 'b', 'TYPE' => $img->mimeType()));
-                        } else {
-                                App::log('Unable to parse the image provided by Google. ', \OCP\Util::WARN);
-                        }
-                }
+	/**
+	 * Fill the string properties in a one go
+	 * @param array $contact
+	 * @param Sabre\VObject\Component $vcard
+	 * @return Sabre\VObject\Component
+	 */
+	protected static function _addPropertyStrings($contact, $vcard) {
 
-				                return $vcard;
-        }
-
-        /**
-         * Fill the string properties in a one go
-         * @param array $contact
-         * @param Sabre\VObject\Component $vcard
-         * @return Sabre\VObject\Component
-         */
-        protected static function _addPropertyStrings($contact, $vcard) {
-
-                foreach (self::$_propertyStrings as $property) {
-                        if (isset($contact[$property])) {
-                                $vcard->setString($property, $contact[$property]);
-                        }
-                }
-                return $vcard;
-        }
+		foreach (self::$_propertyStrings as $property) {
+			if (isset($contact[$property])) {
+				$vcard->setString($property, $contact[$property]);
+			}
+		}
+		return $vcard;
+	}
 
 }
